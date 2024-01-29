2ea3e8f72007c8be4a4a6b9d73f7c5d77613a24a
Ria Singh <riasingh.rs12@gmail.com>
2024-01-28 21:22:29 -0500

    cs 506 exercise part a

---
+++ polynomial.py
@@ -0,0 +1,39 @@
+class X:
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return "X"
+
+class Int:
+    def __init__(self, i):
+        self.i = i
+    
+    def __repr__(self):
+        return str(self.i)
+
+class Add:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def __repr__(self):
+        return repr(self.p1) + " + " + repr(self.p2)
+
+class Mul:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def __repr__(self):
+        if isinstance(self.p1, Add) or isinstance(self.p1, Sub):
+            if isinstance(self.p2, Add) or isinstance(self.p2, Sub):
+                 return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
+            return "( " + repr(self.p1) + " ) * " + repr(self.p2)
+        if isinstance(self.p2, Add) or isinstance(self.p12, Sub):
+            return repr(self.p1) + " * ( " + repr(self.p2) + " )"
+        return repr(self.p1) + " * " + repr(self.p2)
+
+
+poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
+print(poly)
