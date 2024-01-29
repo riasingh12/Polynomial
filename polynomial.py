29e20823c9eda6ba5f6281e780a64644dae91c44
Ria Singh <riasingh.rs12@gmail.com>
2024-01-28 21:27:27 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -4,22 +4,26 @@ class X:
 
     def __repr__(self):
         return "X"
+    
+class Equation:
+    def evaluate(self, i):
+        return eval(repr(self).replace("X", str(i)))
 
-class Int:
+class Int(Equation):
     def __init__(self, i):
         self.i = i
     
     def __repr__(self):
         return str(self.i)
 
-class Add:
+class Add(Equation):
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
-
+    
 class Sub(Equation):
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -28,6 +32,20 @@ class Sub(Equation):
     def __repr__(self):
         return repr(self.p1) + " - " + repr(self.p2)
 
+class Mul(Equation):
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def __repr__(self):
+        if isinstance(self.p1, Add) or isinstance(self.p1, Sub):
+            if isinstance(self.p2, Add) or isinstance(self.p2, Sub):
+                 return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
+            return "( " + repr(self.p1) + " ) * " + repr(self.p2)
+        if isinstance(self.p2, Add) or isinstance(self.p2, Sub):
+            return repr(self.p1) + " * ( " + repr(self.p2) + " )"
+        return repr(self.p1) + " * " + repr(self.p2)
+    
 class Div(Equation):
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -42,21 +60,6 @@ class Div(Equation):
             return repr(self.p1) + " / ( " + repr(self.p2) + " )"
         return repr(self.p1) + " / " + repr(self.p2)
 
-class Mul:
-    def __init__(self, p1, p2):
-        self.p1 = p1
-        self.p2 = p2
-    
-    def __repr__(self):
-        if isinstance(self.p1, Add) or isinstance(self.p1, Sub):
-            if isinstance(self.p2, Add) or isinstance(self.p2, Sub):
-                 return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
-            return "( " + repr(self.p1) + " ) * " + repr(self.p2)
-        if isinstance(self.p2, Add) or isinstance(self.p12, Sub):
-            return repr(self.p1) + " * ( " + repr(self.p2) + " )"
-        return repr(self.p1) + " * " + repr(self.p2)
-
-
-
-poly = Div(Add(Add(Int(4), Int(3)), Add(X(), Mul(Int(1), Add(Mul(X(), X()), Int(1))))), Int(2))
+poly = Sub( Div( Int(4), Sub(Int(3), Mul(Int(1), Int(6)))), Add( X(), Div( Int(1), Sub( Div(X(), Int(2)), Int(3)))))
 print(poly)
+print(poly.evaluate(-1))
